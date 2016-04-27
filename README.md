60bee1794e850b767203d249820de532fade53b5
luis <luis.gonzalez@moncuadro.es>
2016-04-27 09:44:11 +0200

    README improved, removed unuseful code

--- README.md
+++ README.md
@@ -13,17 +13,42 @@ Import jquery and jquery-loading.js to your web app.
 
 ## Use
 
+### Start 
+
 To start showing the loading message, just type
 
-```
+```javascript
 $().startLoading(options);
 
 ```
 
-### Options
+A loading message will be shown after that.
+
+#### Options
 
 Allowed options and its default values are the following
 
 Option | Type | Default | Description
 -------|------|---------|-------------
 message | string | Loading... | The text shown
+message | string | Loading... | The text shown
+message | string | Loading... | The text shown
+
+### Stop
+
+To stop showing the message:
+
+```javascript
+$().stopLoading(options, callback)
+
+```
+
+The current loading message will be hidden after that.
+
+#### Options
+
+Allowed options and its default values are the following
+
+Option | Type | Default | Description
+-------|------|---------|-------------
+delay | integer | 0 | The time in seconds before the message is hidden.
--- jquery-loading.js
+++ jquery-loading.js
@@ -1,40 +1,53 @@
+/**
+ * @desc it generates a black fixed div which fits the whole screen
+ * @param object $options - An object which could contains the message associated to a key named message.
+ *
+*/
 
-	$.fn.startLoading = function(options){
-
-		options = options || {};
-
-		var text = options.text || 'Loading...'; 
-		var text_node = document.createTextNode(text);
-
-		var spin = options.spin === true ? ' fa-spin' : '';
-
-		var icon = 'fa fa-' + (options.icon || 'refresh') + spin;
-		var icon_node = document.createElement('i');
-		icon_node.setAttribute('class', icon)
+$.fn.startLoading = function(options){
 
 
-		var load_wrapper = document.createElement('div');
-		load_wrapper.setAttribute('class', 'load-wrapper');
+	options = options || {};
 
-		var message = document.createElement('div');
-		message.setAttribute('class', 'text');
-		message.appendChild(text_node);
-		message.appendChild(icono_node);
+	// a text node is created with the given message or default.
+	var text = options.text || 'Loading...'; 
+	var text_node = document.createTextNode(text);
 
-		load_wrapper.appendChild(message);
+	// the wrapper in which the message will be printed 
+	var load_wrapper = document.createElement('div');
+	load_wrapper.setAttribute('class', 'load-wrapper');
 
-		$('body').append(load_wrapper);
-	};
+	// the container for the text
+	var message = document.createElement('div');
+	message.setAttribute('class', 'text');
+	message.appendChild(text_node);
 
-	$.fn.stopLoading = function(options, callback){
+	// the div is appended to the wrapper
+	load_wrapper.appendChild(message);
 
-		options = options || {};
+	// The wrapper is appended to the body
+	$('body').append(load_wrapper);
+};
 
-		var delay = options.delay || 0;
 
-		setTimeout(function(){
-			$('body .load-wrapper').remove();
-			callback();
-		}, delay)
-	}
-	
+
+
+/**
+ * @desc it stops showing the message
+ * @param object $options - An object which could contains the delay in seconds for stoping the message.
+ * @param function $callaback - A function to be called after the delay time.  
+ *
+*/
+$.fn.stopLoading = function(options, callback){
+
+	options = options || {};
+
+	var delay = options.delay || 0;
+
+	// After the delay, the wrapper is removed and the callback is called.
+	setTimeout(function(){
+		$('body .load-wrapper').remove();
+		callback();
+	}, delay)
+}
+
